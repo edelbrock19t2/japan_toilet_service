de19d299a7c4917dd98e2e0000fcff6cb6b2e9de
Sebastian Koźluk <s.kozluk@xopero.com>
2023-12-13 08:47:11 +0100

    first commit

---
+++ src/main/java/com/personalProject/japan_toilet_service/JapanToiletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.personalProject.japan_toilet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanToiletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanToiletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalProject/japan_toilet_service/JapanToiletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.japan_toilet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapanToiletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
